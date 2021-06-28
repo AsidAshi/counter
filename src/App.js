b2b4863aeee65dc4e9368296159c3666ca0ab3df
Asid-Ashi <asidashi98@gmail.com>
2021-06-28 15:17:51 +0530

    counter project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useEffect, useState } from "react";
+
+
+
+const App = () => {
+
+  const [time, setTime] = useState(null);
+  const [run, setrun] = useState(false)
+  
+
+  useEffect(() => {
+    if (run) {
+      const intervalID = setInterval(() => {
+        setTime(time - 1)
+
+      }, 1000)
+      if (time === 0) {
+        setrun(false)
+        clearInterval(intervalID)
+      }
+      return () => clearInterval(intervalID)
+    }
+
+
+
+  }, [time, run])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App-link'>
+      <header className='App-header'>  COUNT DOWN TIMER
+        <input className='inputStyle' value={time} onChange={(e) => { setTime(e.target.value) }}></input>
+        <button className='buttonStyleA' onClick={() => { setrun(true) }}>START</button>
+        count:{time}
+        <button className='buttonStyle' onClick={()=>{setrun(false)}}>STOP</button>
+        <button className='buttonStyleB' onClick= { ()=>{setTime(null)}}>RESET</button>
+
       </header>
+      
     </div>
-  );
+  )
 }
+
+
+
 
 export default App;
